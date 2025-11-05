53069cf616b054ae8485a20d101f6af68e5685db
Ahmed Ibrahim <ahmedibraiii2000@gmail.com>
2025-11-05 23:26:36 +0100

    initail commit

---
+++ src/main/java/com/example/squizzbattle/SquizzBattleApplication.java
@@ -0,0 +1,13 @@
+package com.example.squizzbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquizzBattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquizzBattleApplication.class, args);
+    }
+
+}
