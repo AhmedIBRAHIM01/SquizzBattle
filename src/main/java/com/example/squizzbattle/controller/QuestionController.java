67933bcc31a50415032d004567c10d8be80acaf7
Ahmed Ibrahim <ahmedibraiii2000@gmail.com>
2025-11-11 22:36:06 +0100

    implement controller classes

--- src/main/java/com/example/squizzbattle/controller/QuestionController.java
+++ src/main/java/com/example/squizzbattle/controller/QuestionController.java
@@ -1,2 +1,59 @@
-package com.example.squizzbattle.controller;public class QuestionController {
+package com.example.squizzbattle.controller;
+
+import com.example.squizzbattle.model.Question;
+import com.example.squizzbattle.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/questions")
+@CrossOrigin(origins = "*")
+public class QuestionController {
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService){
+        this.questionService=questionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Question> saveQuestion(
+            @RequestParam Question question){
+        Question question1= questionService.addQuestion(question);
+        return ResponseEntity.ok(question1);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Question>> getQuestions(){
+        List<Question> questions= questionService.getAllQuestions();
+        return ResponseEntity.ok(questions);
+    }
+
+    @GetMapping("/random")
+    public ResponseEntity<Question> getRandomQuestion(){
+        Question question= questionService.getRandomQuestion();
+        return ResponseEntity.ok(question);
+    }
+    @GetMapping("/{questionId}")
+    public ResponseEntity<Question> getQuestionById(@PathVariable Long questionId){
+        Optional<Question> question= questionService.getQuestionById(questionId);
+        return question.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{questionId}")
+    public ResponseEntity<Void> deleteQuestionById(@PathVariable Long questionId){
+       questionService.deleteQuestion(questionId);
+       return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/example/squizzbattle/controller/UserController.java
+++ src/main/java/com/example/squizzbattle/controller/UserController.java
@@ -1,2 +1,62 @@
-package com.example.squizzbattle.controller;public class UserController {
+package com.example.squizzbattle.controller;
+
+
+import com.example.squizzbattle.model.User;
+import com.example.squizzbattle.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/Users")
+@CrossOrigin(origins = "*")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService= userService;
+    }
+
+    // post Methods
+    @PostMapping
+    public ResponseEntity<Void> saveUser(@RequestParam User user){
+        userService.saveUser(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    // get Methods
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> users= userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/id/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable Long userId){
+        Optional<User> user= userService.getUserById(userId);
+        return user.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/name/{userName}")
+    public ResponseEntity<User> getUserByName(@PathVariable String userName){
+        Optional<User> user= userService.getUserByName(userName);
+        return user.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // delete Methods
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
